30726aba288bbccc95bd94910d82b133cb875c4b
Begnini <josiane.begnini@capgemini.com>
2025-10-22 11:01:42 -0300

    teste

---
+++ ms-cartao/src/main/java/com/example/gw_gerenciador_cartoes/MsCartaoApplication.java
@@ -0,0 +1,7 @@
+package com.example.gw_gerenciador_cartoes;
+@SpringBootApplication
+public class MsCartaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsCartaoApplication.class, args);
+    }
+}
