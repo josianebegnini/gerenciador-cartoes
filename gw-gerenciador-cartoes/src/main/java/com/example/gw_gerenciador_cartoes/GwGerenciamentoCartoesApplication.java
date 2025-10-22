71820be6c9562e4650093b099506bc732826f8d3
Josiane Begnini <josianebegnini@gmail.com>
2025-10-22 10:10:05 -0300

    gateway

---
+++ gw-gerenciador-cartoes/src/main/java/com/example/gw_gerenciador_cartoes/GwGerenciamentoCartoesApplication.java
@@ -0,0 +1,10 @@
+package com.example.gw_gerenciador_cartoes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwGerenciamentoCartoesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GwGerenciamentoCartoesApplication.class, args);
+    }
+}
