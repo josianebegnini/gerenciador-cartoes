e8dc940dcd383f77f03cf9303d4dbe9a457defbb
vit0ur <vitor.engenhariapro@gmail.com>
2025-10-23 15:16:40 -0300

    WIP: alterações em app.routes.ts

--- gerenciador-cartoes-front/src/app/app.routes.ts
+++ gerenciador-cartoes-front/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { CadastroCliente } from './components/cadastro-cliente/cadastro-cliente';
+import { CadastroCartaoComponent } from './components/cadastro-cartao/cadastro-cartao';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: '**', redirectTo: '' }
+  { path: 'home', component: Home },
+  { path: 'cadastro-cliente', component: CadastroCliente },
+  { path: 'cadastro-cartao', component: CadastroCartaoComponent },
+  { path: '**', redirectTo: 'home' },
 ];
