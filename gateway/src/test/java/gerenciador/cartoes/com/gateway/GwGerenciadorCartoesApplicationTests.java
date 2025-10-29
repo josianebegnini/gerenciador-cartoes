96a3f0e28e67e2c7944c448cfb92d1d99e8fabc3
Jose Vitor Oliveira de Araujo <vitor.engenhariapro@gmail.com>
2025-10-29 09:20:48 -0300

    fix/bug: ajustes do build de serviços para testes locais

--- gateway/src/test/java/gerenciador/cartoes/com/gateway/GwGerenciadorCartoesApplicationTests.java
+++ gateway/src/test/java/gerenciador/cartoes/com/gateway/GwGerenciadorCartoesApplicationTests.java
@@ -1,8 +1,10 @@
-package com.example.gw_gerenciador_cartoes;
+package gerenciador.cartoes.com.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration
 @SpringBootTest
 public class GwGerenciadorCartoesApplicationTests {
     @Test
