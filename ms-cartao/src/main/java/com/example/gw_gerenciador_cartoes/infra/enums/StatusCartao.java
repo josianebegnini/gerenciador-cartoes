9254db69d61c127407731a9b79bbfe5ad968ea5a
kamilasst <kamilasantosdev@gmail.com>
2025-10-31 13:18:05 -0300

    feat: aplicar deserialização case-insensitive nos enums StatusCartao, TipoCartao e TipoEmissao

--- ms-cartao/src/main/java/com/example/gw_gerenciador_cartoes/infra/enums/StatusCartao.java
+++ ms-cartao/src/main/java/com/example/gw_gerenciador_cartoes/infra/enums/StatusCartao.java
@@ -1,9 +1,22 @@
 package com.example.gw_gerenciador_cartoes.infra.enums;
 
+import com.example.gw_gerenciador_cartoes.infra.exception.MensagensErroConstantes;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum StatusCartao {
     DESATIVADO,
     ATIVADO,
     BLOQUEADO,
     REJEITADO,
-    CANCELADO
+    CANCELADO;
+
+    @JsonCreator
+    public static StatusCartao from(String value) {
+        for (StatusCartao status : StatusCartao.values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException(MensagensErroConstantes.CARTAO_STATUS_CARTAO_INVALIDO + value);
+    }
 }
