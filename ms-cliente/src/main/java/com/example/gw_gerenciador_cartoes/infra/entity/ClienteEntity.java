4157d2044dbaef3a58c856402664f69d42e92892
TomSpinelli <tomasspinelli@gmail.com>
2025-10-31 19:32:55 -0300

    commit testes

--- ms-cliente/src/main/java/com/example/gw_gerenciador_cartoes/infra/entity/ClienteEntity.java
+++ ms-cliente/src/main/java/com/example/gw_gerenciador_cartoes/infra/entity/ClienteEntity.java
@@ -11,7 +11,7 @@ public class ClienteEntity {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotBlank(message = "Nome é obrigatório")
